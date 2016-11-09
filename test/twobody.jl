57e4517dd160e7f30f2177d6090e60e7d0f3faa5
Takeshi Nishimatsu <t-nissie@imr.tohoku.ac.jp>
2016-11-09 12:08:55 +0900

    velocity verlet and leapfrog give similar results

--- test/twobody.jl
+++ test/twobody.jl
@@ -19,11 +19,12 @@ p0 = [ 0.0,-0.2,  0.0,0.2]
 t0 = 0.0
 h  = 0.1
 n  = 400
+p_half = p0 + h/2 .* map(f -> f(q0), pdot)
 
 t_eu,q_eu,p_eu = time_evolution(          euler, qdot, pdot, q0, p0, t0, h, n)
 t_vv,q_vv,p_vv = time_evolution(velocity_verlet, qdot, pdot, q0, p0, t0, h, n)
 t_pv,q_pv,p_pv = time_evolution(position_verlet, qdot, pdot, q0, p0, t0, h, n)
-t_lf,q_lf,p_lf = time_evolution(       leapfrog, qdot, pdot, q0, p0, t0, h, n)
+t_lf,q_lf,p_lf = time_evolution(       leapfrog, qdot, pdot, q0, p_half, t0, h, n)
 
 @test_approx_eq_eps q_eu[1,  2] q_vv[1,  2] 1.0e-3
 @test_approx_eq_eps q_eu[2,  2] q_vv[2,  2] 1.0e-3
@@ -35,14 +36,19 @@ t_lf,q_lf,p_lf = time_evolution(       leapfrog, qdot, pdot, q0, p0, t0, h, n)
 @test_approx_eq_eps q_pv[3,n+1] q_vv[3,n+1] 1.0e-4
 @test_approx_eq_eps q_pv[4,n+1] q_vv[4,n+1] 1.0e-4
 
-# @test_approx_eq_eps q_lf[1,n+1] q_vv[1,n+1] 1.0e-4
-# @test_approx_eq_eps q_lf[2,n+1] q_vv[2,n+1] 1.0e-4
-# @test_approx_eq_eps q_lf[3,n+1] q_vv[3,n+1] 1.0e-4
-# @test_approx_eq_eps q_lf[4,n+1] q_vv[4,n+1] 1.0e-4
+@test_approx_eq_eps q_lf[1,n+1] q_vv[1,n+1] 1.0e-13
+@test_approx_eq_eps q_lf[2,n+1] q_vv[2,n+1] 1.0e-13
+@test_approx_eq_eps q_lf[3,n+1] q_vv[3,n+1] 1.0e-13
+@test_approx_eq_eps q_lf[4,n+1] q_vv[4,n+1] 1.0e-13
 
 using Winston
 plot(q_pv[1,:],q_pv[2,:],"r-", q_pv[1,1:20:n],q_pv[2,1:20:n],"ro",
      q_pv[3,:],q_pv[4,:],"g--",q_pv[3,1:20:n],q_pv[4,1:20:n],"gs")
 xlim(-5.0,7.0)
 ylim(-6.0,6.0)
-savefig("twobody.eps")
+savefig("twobody_pv.eps")
+plot(q_lf[1,:],q_lf[2,:],"r-", q_lf[1,1:20:n],q_lf[2,1:20:n],"ro",
+     q_lf[3,:],q_lf[4,:],"g--",q_lf[3,1:20:n],q_lf[4,1:20:n],"gs")
+xlim(-5.0,7.0)
+ylim(-6.0,6.0)
+savefig("twobody_lf.eps")
