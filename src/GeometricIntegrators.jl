3e142bb4f7e921982b54f02fe745daa85ef131fe
Takeshi Nishimatsu <t-nissie@imr.tohoku.ac.jp>
2016-11-11 10:31:14 +0900

    refactoring

--- src/GeometricIntegrators.jl
+++ src/GeometricIntegrators.jl
@@ -21,71 +21,66 @@
 ##
 module GeometricIntegrators
 
-function           euler{T<:AbstractFloat}(qdot,   # It was qdot::Array{Function,1}.
-                                           pdot,
+function            euler{T<:AbstractFloat}(qdot,   # It was qdot::Array{Function,1}.
+                                            pdot,
                                               q::Array{T,1},
                                               p::Array{T,1},
-                                              t::AbstractFloat,
-                                              h::AbstractFloat)
+                                              h::T)
     q_next = q      + h   .* map(f -> f(p), qdot)
     p_next = p      + h   .* map(f -> f(q), pdot)
-    return t + h, q_next, p_next
+    return q_next, p_next
 end
 
 function symplectic_euler{T<:AbstractFloat}(qdot,
                                             pdot,
                                               q::Array{T,1},
                                               p::Array{T,1},
-                                              t::AbstractFloat,
-                                              h::AbstractFloat)
+                                              h::T)
     q_next = q      + h   .* map(f -> f(p),      qdot)
     p_next = p      + h   .* map(f -> f(q_next), pdot)
-    return t + h, q_next, p_next
+    return q_next, p_next
 end
 
-function velocity_verlet{T<:AbstractFloat}(qdot,
-                                           pdot,
+function  velocity_verlet{T<:AbstractFloat}(qdot,
+                                            pdot,
                                               q::Array{T,1},
                                               p::Array{T,1},
-                                              t::AbstractFloat,
-                                              h::AbstractFloat)
+                                              h::T)
     p_half = p      + h/2 .* map(f -> f(q),      pdot)
     q_next = q      + h   .* map(f -> f(p_half), qdot)
     p_next = p_half + h/2 .* map(f -> f(q_next), pdot)
-    return t + h, q_next, p_next
+    return q_next, p_next
 end
 
-function position_verlet{T<:AbstractFloat}(qdot,
-                                           pdot,
+function  position_verlet{T<:AbstractFloat}(qdot,
+                                            pdot,
                                               q::Array{T,1},
                                               p::Array{T,1},
-                                              t::AbstractFloat,
-                                              h::AbstractFloat)
+                                              h::T)
     q_half = q      + h/2 .* map(f -> f(p),      qdot)
     p_next = p      + h   .* map(f -> f(q_half), pdot)
     q_next = q_half + h/2 .* map(f -> f(p_next), qdot)
-    return t + h, q_next, p_next
+    return q_next, p_next
 end
 
-function        leapfrog{T<:AbstractFloat}(qdot,
-                                           pdot,
+function         leapfrog{T<:AbstractFloat}(qdot,
+                                            pdot,
                                               q::Array{T,1},
                                               p::Array{T,1},
-                                              t::AbstractFloat,
-                                              h::AbstractFloat)
+                                              h::T)
     q_next = q      + h   .* map(f -> f(p),      qdot)
     p_next = p      + h   .* map(f -> f(q_next), pdot)
-    return t + h, q_next, p_next
+    return q_next, p_next
 end
 
-function time_evolution{T<:AbstractFloat}(method,
-                                          qdot,
-                                          pdot,
-                                            q0::Array{T,1},
-                                            p0::Array{T,1},
-                                            t0::AbstractFloat,
-                                             h::AbstractFloat,
-                                             n::Integer)
+function   time_evolution{T<:AbstractFloat}(method::Function,
+                                            qdot,
+                                            pdot,
+                                             q0::Array{T,1},
+                                             p0::Array{T,1},
+                                             t0::T,
+                                              h::T,
+                                              n::Integer)
     q = zeros(length(q0), n+1)
     p = zeros(length(p0), n+1)
     t = zeros(1, n+1)
@@ -93,7 +88,8 @@ function time_evolution{T<:AbstractFloat}(method,
     p[:,1] = p0
     t[1] = t0
     for ii in 2:(n+1)
-        t[ii], q[:,ii], p[:,ii] = method(qdot, pdot, q[:,ii-1], p[:,ii-1], t[ii-1], h)
+        t[ii] = t0 + h * (ii - 1)
+        q[:,ii], p[:,ii] = method(qdot, pdot, q[:,ii-1], p[:,ii-1], h)
     end
     return t,q,p
 end
@@ -105,7 +101,7 @@ if isdefined(:PROGRAM_FILE) && PROGRAM_FILE == basename(@__FILE__)
    using GeometricIntegrators
 
    function energy{T<:AbstractFloat}(q::Array{T,2},
-                                     p::Array{T,2}, m, k, n::Integer)
+                                     p::Array{T,2}, m::T, k::T, n::Integer)
         e = zeros(Float64, n+1)
         for i in 1:n+1
             e[i] = dot(p[:,i],p[:,i])/2/m +
